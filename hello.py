4bb7fa9bfb8cc71b8de9a4b37689f4a9219c14f2
Emiliano Agraz <emilianoagraz@BanBansMacBook.home>
2022-06-01 20:33:57 -0500

    added hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+# Pedir nombre
+nombre = input("¿Cómo te llamas?\n\n").strip().title()
+
+# Saludo
+print("\n¡Hola puto!")
+print("\n¿A poco creías que te iba a llamar" ,  nombre + "?" , "mimimi" "\n")
