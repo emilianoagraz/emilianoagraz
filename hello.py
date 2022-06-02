8a0599fea24a5882660963805f08a501bd5de8a0
Emiliano Agraz <emilianoagraz@BanBansMacBook.home>
2022-06-01 20:40:47 -0500

    escroto

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@
 nombre = input("¿Cómo te llamas?\n\n").strip().title()
 
 # Saludo
-print("\n¡Hola puto!")
+print("\n¡Hola cara de escroto!")
 print("\n¿A poco creías que te iba a llamar" ,  nombre + "?" , "mimimi" "\n")
